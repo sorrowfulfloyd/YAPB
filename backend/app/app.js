41ac25f752f6afe33c3f1a0eae2480938a0b4a76
sorrowfulfloyd <gogercin.nurullah@gmail.com>
2024-01-11 16:48:09 +0300

    advanced document controls(sıralama, filtreleme, sadece istenen alanları gönderme)

--- backend/app/app.js
+++ backend/app/app.js
@@ -1,13 +1,13 @@
-const express = require('express');
-require('express-async-errors')
+const express = require("express");
+require("express-async-errors");
 const app = express();
-const bookRoute = require('../routes/book');
+const bookRoute = require("../routes/book");
 
-const notFound = require('../middleware/notFound');
-const errorHandler = require('../middleware/errorHandler')
+const notFound = require("../middleware/notFound");
+const errorHandler = require("../middleware/errorHandler");
 
 app.use(express.json());
-app.use('/api/v1/books', bookRoute);
+app.use("/api/v1/books", bookRoute);
 app.use(errorHandler);
 app.use(notFound);
 
--- backend/controller/bookController.js
+++ backend/controller/bookController.js
@@ -1,66 +1,126 @@
-const Book = require('../models/model')
-const replaceTurkishChars = require('../helpers/replaceTurkishChars')
+const Book = require("../models/model");
+const replaceTurkishChars = require("../helpers/replaceTurkishChars");
 
 const getOneBook = async (req, res) => {
-  const singleBookObj = await Book.findById(req.params.id);
+	const singleBookObj = await Book.findById(req.params.id);
 
-  return singleBookObj ?
-    res.status(200).json({
-      'İstediğiniz obje bulundu!': { Kitap: singleBookObj }
-    })
-    : res.status(404).json({ Error: 'Verilen id ile veritabanında bir obje bulunamadı', id: req.params.id });
-}
+	return singleBookObj
+		? res.status(200).json({
+				"İstediğiniz obje bulundu!": { Kitap: singleBookObj },
+		  })
+		: res.status(404).json({
+				Error: "Verilen id ile veritabanında bir obje bulunamadı",
+				id: req.params.id,
+		  });
+};
 
 const getAllBooks = async (req, res) => {
-  const {title, author} = req.query;
-  const newQuery = {}
+	const { title, author, sort, fields, numericFilters } = req.query;
+	const newQuery = {};
 
-  if (title) {
-     newQuery.title = replaceTurkishChars(title);
-  }
+	if (title) {
+		newQuery.title = replaceTurkishChars(title);
+	}
 
-  if (author) {
-    newQuery.author = replaceTurkishChars(author);
-  }
+	if (author) {
+		newQuery.author = replaceTurkishChars(author);
+	}
 
-  const page = Number(req.query.params) || 1;
-  const limit = Number(req.query.params) || 99;
-  const skipCount = (page - 1) * limit;
-  
-  console.log(newQuery)  
+	if (numericFilters) {
+		const operatorMap = {
+			">": "$gt",
+			">=": "$gte",
+			"=": "$eq",
+			"!=": "$ne", // ayriyeten not equal comparator'unu ekledim
+			"<": "$lt",
+			"<=": "$lte",
+		};
+		const regexStr = /\b(<|>|>=|=|<|<=|!=)\b/g;
+		let filters = numericFilters.replace(
+			regexStr,
+			(match) => `-${operatorMap[match]}-`,
+		);
+		const options = ["price", "stock", "edition"];
 
-  const allBooks = await Book.find(newQuery).skip(skipCount).limit(limit);
-  return res.status(200).json({ 'Bütün kitapların sayısı': allBooks.length, Kitaplar: allBooks })
-}
+		filters = filters.split(",");
+
+		for (const item of filters) {
+			const [field, operator, value] = item.split("-");
+			if (options.includes(field)) {
+				newQuery[field] = { [operator]: Number(value) };
+			}
+		}
+	}
+
+	console.log("şu ana kadar oluşturulan obje: ", newQuery);
+
+	let result = Book.find(newQuery);
+
+	if (sort) {
+		const sortList = sort.split(",").join(" ");
+		result = result.sort(sortList);
+	} else {
+		result = result.sort("dateAdded");
+	}
+
+	if (fields) {
+		const fieldsList = fields.split(",").join(" ");
+		result.select(fieldsList);
+	}
+
+	const page = Number(req.query.params) || 1;
+	const limit = Number(req.query.params) || 99;
+	const skipCount = (page - 1) * limit;
+
+	result = result.skip(skipCount).limit(limit);
+
+	const allBooks = await result;
+
+	// console.log(result);
+
+	return res
+		.status(200)
+		.json({ "Bütün kitapların sayısı": allBooks.length, Kitaplar: allBooks });
+};
 
 const postABook = async (req, res) => {
+	const newBook = new Book({ ...req.body });
+	const book = await newBook.save();
 
-  const newBook = new Book({ ...req.body });
-  const book = await newBook.save();
-
-  return res.status(201).json({
-    Status: '201 - Oluşturuldu',
-    message: 'Gönderdiğiniz obje ile veritabanında yeni bir kitap başarıyla oluşturuldu.',
-    "Oluşturulan Objeniz": book
-  });
-}
+	return res.status(201).json({
+		Status: "201 - Oluşturuldu",
+		message:
+			"Gönderdiğiniz obje ile veritabanında yeni bir kitap başarıyla oluşturuldu.",
+		"Oluşturulan Objeniz": book,
+	});
+};
 
 const updateBook = async (req, res) => {
+	const book = await Book.findByIdAndUpdate(req.params.id, req.body, {
+		new: true,
+		runValidators: true,
+	});
 
-  const book = await Book.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true })
-
-  return book
-    ? res.status(200).json({ message: "Kitap objesi başarıyla güncellendi", yeniObje: book })
-    : res.status(404).json({ message: "Böyle bir obje bulunamadı", "örnek obje isteği": exampleRequest });
-}
+	return book
+		? res
+				.status(200)
+				.json({ message: "Kitap objesi başarıyla güncellendi", yeniObje: book })
+		: res.status(404).json({
+				message: "Böyle bir obje bulunamadı",
+		  });
+};
 
 const deleteBook = async (req, res) => {
+	const book = await Book.findByIdAndDelete(req.params.id);
 
-  const book = await Book.findByIdAndDelete(req.params.id);
+	return book
+		? res.status(200).json({
+				message: "Gönderdiğiniz id ile bulunan obje başarıyla silindi.",
+		  })
+		: res.status(404).json({
+				message: "Verilen id ile bir obje bulunamadı",
+				id: req.params.id,
+		  });
+};
 
-  return book
-    ? res.status(200).json({ message: "Gönderdiğiniz id ile bulunan obje başarıyla silindi." })
-    : res.status(404).json({ message: "Verilen id ile bir obje bulunamadı", id: req.params.id })
-}
-
-module.exports = { getOneBook, getAllBooks, postABook, updateBook, deleteBook }
+module.exports = { getOneBook, getAllBooks, postABook, updateBook, deleteBook };
